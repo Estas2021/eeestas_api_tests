c757fe7680fbba55845f566d428bdfa3634f5399
Stas <88334645+Estas2021@users.noreply.github.com>
2025-09-10 15:26:28 +0300

    вынес методы в функции классов, которые создал, создал экземпляры классов для обращения к объектам через методы классов

--- tests/functional/test_post_v1_account.py
+++ tests/functional/test_post_v1_account.py
@@ -1,6 +1,9 @@
-import pprint
 import json
-import requests
+
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
+from api_mailhog.apis.mailhog_api import MailhogApi
+
 
 # from main import response_json
 
@@ -8,7 +11,11 @@ import requests
 def test_post_v1_account():
     # register user
 
-    login = 'eeestas3'
+    account_api = AccountApi(host='http://5.63.153.31:5051')
+    login_api = LoginApi(host='http://5.63.153.31:5051')
+    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+
+    login = 'eeestas6'
     password = 'eeestas3'
     email = f'{login}@mail.ru'
 
@@ -18,52 +25,31 @@ def test_post_v1_account():
         'email': email,
     }
 
-    response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
+    response = account_api.post_v1_account(json_data=json_data)
     print(response.status_code)
     print(response.text)
     assert response.status_code == 201, "Пользак не был зарегистрирован"
 
-
     # get registration messages via email
 
-    params = {
-        'limit': '50',
-    }
-
-    response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
+    response = mailhog_api.get_api_v2_messages()
     print('\n', response.status_code)
-    # print(response.text)
+    print(response.text)
     assert response.status_code == 200, "Письма не были получены"
 
-
-
-
     # get activation token
-    token = None
-
-    for item in response.json()['items']:
-        user_data = json.loads(item['Content']['Body'])
-        user_login = user_data['Login']
-        if user_login == login:
-            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-            print('token: ', token)
+    token = get_activation_token_by_login(login, response)
 
     assert token is not None, f"Токен для пользака {login} не был получен"
 
-
     # activate user
-    headers = {
-        'accept': 'text/plain',
-    }
-
-    response = requests.put(f'http://5.63.153.31:5051/v1/account/{token}', headers=headers)
+    response = account_api.put_v1_account_token(token)
     print(response.status_code)
     print(response.text)
 
     assert response.status_code == 200, f"Пользак {login} не был активирован"
 
-
-    # login
+    # login user
 
     json_data = {
         'login': login,
@@ -71,7 +57,28 @@ def test_post_v1_account():
         'rememberMe': True,
     }
 
-    response = requests.post('http://5.63.153.31:5051/v1/account/login', headers=headers, json=json_data)
+    response = login_api.post_v1_account_login(json_data=json_data)
     print(response.status_code)
     print(response.text)
     assert response.status_code == 200, f"Пользак {login} не был авторизован"
+
+
+
+def get_activation_token_by_login(
+        login,
+        response
+):
+    token = None
+    for item in response.json()['items']:
+        user_data = json.loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            print('token: ', token)
+    return token
+
+
+
+
+
+
